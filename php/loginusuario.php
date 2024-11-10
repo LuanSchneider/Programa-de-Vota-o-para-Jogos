b57e10ba09f8c6c2e5394d8cfe36e072cbb34f28
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2024-11-10 18:06:19 -0300

    save

---
+++ php/loginusuario.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+$conn = mysqli_connect("localhost", "admin", "admin", "avaliajogos");
+
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+
+$nome_usuario = $_POST['usuario_nome'];
+$senha_usuario = $_POST['usuario_senha'];
+
+$sql = "SELECT * FROM usuarios WHERE nome = ? AND senha = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $nome_usuario, $senha_usuario);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    header("Location: ../votacao.html");
+    exit();
+} else {
+    echo "<script>alert('Nome de usuário ou senha incorretos'); window.location.href='../index.html';</script>";
+}
+
+$stmt->close();
+$conn->close();
+?>
